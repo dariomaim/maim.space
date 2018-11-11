49e34e3415ec5614d9d39517ab509c6fd6fa5498
Dario <30393370+dariomaim@users.noreply.github.com>
2018-11-11 20:29:49 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+		
+		<meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>" charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+        
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+		 
+		<?php wp_head(); ?>
+	
+	</head>
+	
+	<body <?php body_class(); ?>>
+    
+        <header>
+            
+            <p class="toggle-menu" onclick="document.querySelector('body').classList.toggle('show-menu')"><?php _e( 'Menu', 'davis' ); ?></p>
+
+            <?php if ( has_nav_menu( 'primary-menu' ) ) wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
+
+            <h2><a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></h2>
+
+            <?php if ( get_bloginfo( 'description' ) ) : ?>
+                <p><?php bloginfo( 'description' ); ?></p>
+            <?php endif; ?>
+
+        </header><!-- header -->
+		
+		<div class="wrapper">
+            
+		   <div class="subtitular">		
+			 
+			  <h5><?php echo single_cat_title("", false); ?></h5>
+			     
+			     <?php echo category_description( $category_id ); ?>
+		    
+		   </div>	
+		
+        
+			<?php if ( have_posts() )  : 
+
+                while ( have_posts() ) : the_post(); ?>	
+            
+			      <div <?php post_class( 'post' ); ?>>
+
+                        <?php if ( ! get_post_format() == 'aside' ) : ?>
+
+                            <h1 class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+
+                        <?php endif;
+                        
+                        if ( has_post_thumbnail() ) : ?>
+                        
+                            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="featured-image">
+                                <?php the_post_thumbnail( 'post-image' ); ?>    
+                            </a>
+                            
+                        <?php endif; ?>
+                      
+					  <div class="content">
+                            <?php if ( is_home() || is_archive() ) :  
+                                 the_excerpt();
+						         else :
+						         the_content(); 
+						     endif; ?>
+                                 
+                        </div><!-- .content -->
+                       
+                       
+					  <?php 
+                        
+                        if ( is_singular() ) wp_link_pages();
+
+                        if ( get_post_type() == 'post' ) : ?>
+                          
+					    
+                            <div class="meta">
+
+                                <p>
+                                   
+                                    <a href="#" title="<?php the_title_attribute(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
+                                 <?php 
+									///
+									//
+									/* No meta right now 
+                                    <?php if ( comments_open() ) : ?>
+                                        <span class="sep"></span><?php comments_popup_link( __( 'Add Comment', 'davis' ), __( '1 Comment', 'davis' ), '% ' . __( 'Comments', 'davis' ), '', __( 'Comments off', 'davis' ) ); ?>
+                                    <?php endif; ?>
+                                    
+                                    <?php if ( is_sticky() ) : ?>
+                                        <span class="sep"></span><?php _e( 'Sticky', 'davis' ); ?>
+                                    <?php endif ?>
+
+                                </p>
+                                    */ ?>
+                                <?php if ( is_singular( 'post' ) ) : ?>
+                                
+                                    <p><?php _e( 'en', 'davis' ); ?> <?php the_category( ', ' ); ?></p>
+                                    <p><?php the_tags( ' #', ' #', ' ' ); ?></p>
+                                    
+                                <?php endif; ?>
+                                    
+                            </div><!-- .meta -->
+                      
+					    
+					  
+                        <?php endif;
+                        ///
+                        // No comments right now 
+                        // if ( is_singular() ) comments_template(); ?>
+
+                    </div><!-- .post -->
+
+                    <?php 
+                
+                endwhile;
+           
+		    else : ?>
+
+                <div class="post">
+
+                    <p><?php _e( 'Sorry, the page you requested cannot be found.', 'davis' ); ?></p>
+
+                </div><!-- .post -->
+
+            <?php endif;
+            
+            if ( ( ! is_singular() ) && ( $wp_query->post_count >= get_option( 'posts_per_page' ) ) ) : ?>
+	        
+		        <div class="pagination">
+			        
+					<?php previous_posts_link( '&larr; ' . __( 'más Nuevos', 'davis' ) ); ?>
+					<?php next_posts_link( __( 'más Antigüos', 'davis') . ' &rarr;' ); ?>
+					
+		        </div><!-- .pagination -->
+	        
+	        <?php endif; ?>
+			
+	        <footer>
+		        
+		        <p><?php echo date( 'Y' ); ?></p>
+		        <p><a href="uncopyright">uncopyright</a></p>
+		        
+	        </footer><!-- footer -->
+	        
+	    </div><!-- .wrapper -->
+	    
+	    <?php wp_footer(); ?>
+	        
+	</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== maim.space ===
+Requires at least: 4.4
+Tested up to: 
+Stable tag: trunk
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Installation ==
+
+1. Upload the theme
+2. Activate the theme
+
+All theme specific options are handled through the WordPress Customizer.
+
+
+== Frequently Asked Questions ==
+
+= How do I activate dark mode? =
+Dark Mode displays the site with white text and black background. You can activate it in Appearance > Customize, in the Colors tab.
+
+
+== Licenses ==
+
+PT Serif
+License: SIL Open Font License, 1.1 
+Source: https://fonts.google.com/specimen/PT+Serif
+
+
+== Changelog ==
+
+Version 1.13 (2018-11-11)
+-------------------------
+- Added Gutenberg support
+
+Version 1.11 (2018-09-10)
+-------------------------
+- Code cleanup
+
+Version 1.0 (2018-05-03)
+-------------------------
+- Initial commits
